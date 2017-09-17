5b2e310e0e6a39b149242fe65e4fdfa41d8f10bf
REYJDR <REINALDO.DAOU@GMAIL.COM>
2017-09-16 20:20:52 -0500

    UPDATE

--- ACI_WEB/application/model/model.php
+++ ACI_WEB/application/model/model.php
@@ -802,7 +802,7 @@ return $get_req;
 
 public function get_req_to_report_urge($sort,$limit,$clause){
 
-$sql='SELECT count(*) as cuenta, job FROM `REQ_HEADER` 
+ECHO $sql='SELECT count(*) as cuenta, job FROM `REQ_HEADER` 
 '.$clause.' group by job order by ID '.$sort.' limit '.$limit.';';
 
 $get_req = $this->Query($sql);
