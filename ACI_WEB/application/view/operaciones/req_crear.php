d6e79c90ab9dcc909577a0d9d6ed2a84099ea2ba
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2017-10-01 21:40:12 -0500

    update

--- ACI_WEB/application/view/operaciones/req_crear.php
+++ ACI_WEB/application/view/operaciones/req_crear.php
@@ -552,6 +552,8 @@ function isFutureDate(idate){
 var today = new Date().getTime(),
     idate = idate.split("/");
 
+    console.log(today);
+
 idate = new Date(idate[2], idate[1] - 1, idate[0]).getTime();
 return (today - idate) < 0 ? true : false;
 }
