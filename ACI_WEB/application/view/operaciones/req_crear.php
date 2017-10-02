8b653021951787c0992c27a5af87c747a228492b
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2017-10-01 21:46:47 -0500

    update

--- ACI_WEB/application/view/operaciones/req_crear.php
+++ ACI_WEB/application/view/operaciones/req_crear.php
@@ -553,7 +553,7 @@ var today = new Date().getTime(),
     idate = idate.split("-");
 
 
-idate = new Date(idate[2], idate[1] - 1, idate[0]).getTime();
+idate = new Date(idate[0], idate[1] , idate[2]).getTime();
 return (today - idate) < 0 ? true : false;
 }
 
