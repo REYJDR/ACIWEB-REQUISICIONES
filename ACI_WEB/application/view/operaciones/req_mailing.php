869a57f3f75e3c2b5a43ab1c31d000aa6f9eb646
Reinaldo Daou <reinaldo.daou@gmail.com>
2017-12-07 09:59:49 -0600

    update

--- ACI_WEB/application/view/operaciones/req_mailing.php
+++ ACI_WEB/application/view/operaciones/req_mailing.php
@@ -123,7 +123,7 @@ $value = json_decode($value);
 $message .= '<tr>
    <td width="15%" style="padding-right:10px; text-align: left;">'.$value->{'ProductID'}.'</td>
    <td width="35%" ">'.trim($value->{'DESCRIPCION'}).'</td>
-   <td width="10%" class="numb" style="text-align: center; padding-right">'.number_format($value->{'CANTIDAD'},2).'</td>
+   <td width="10%" class="numb" style="text-align: center; >'.number_format($value->{'CANTIDAD'},2).'</td>
    <td width="10%" style="text-align: center; ">'.$value->{'UNIDAD'}.'</td>
    <td width="10%" style="text-align: center; ">'.$value->{'JOB'}.'</td>
    <td width="10%" style="text-align: center; ">'.$value->{'PHASE'}.'</td>
