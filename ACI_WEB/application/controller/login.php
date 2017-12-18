ab5f1273136c0121372af9d8d40a11dcfd815af1
Reinaldo Daou <reinaldo.daou@gmail.com>
2017-12-18 10:33:37 -0600

    update

--- ACI_WEB/application/controller/login.php
+++ ACI_WEB/application/controller/login.php
@@ -3,14 +3,14 @@
 class login extends Controller
 {
 
-public function index($temp_url){
+public function index($temp_url=''){
 
 
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/login/control.php';
         require APP . 'view/_templates/footer.php';
-    }
+}
 
 
 public function login_out(){
