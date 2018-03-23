58bc11cdca16558ced3f250f314a7f646a4d9a1d
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-03-23 10:29:48 -0600

    update

--- ACI_WEB/application/view/home/edit_account.php
+++ ACI_WEB/application/view/home/edit_account.php
@@ -290,7 +290,7 @@ self.location="'.URL.'index.php?url=home/edit_account/'.$id.'";
 
 <div class="title col-lg-12"></div>
 <div class="col-lg-4">
-<?php if ($INF_rol_2 == 1 && $this->model->active_user_role == 'admin'){ echo $this->model->active_user_role ; ?>
+<?php if ($INF_rol_2 == 1 && $this->model->active_user_role == 'admin'){  ?>
 	
 <a title="modificar Item" data-toggle="modal" data-target="#jobModal"  href="javascript:void(0)" >
 <input type="button" id="modify_button" name="modify_button"  class="btn btn-warning btn-sm btn-icon icon-left" value="Asignar Proyectos"/></a>
