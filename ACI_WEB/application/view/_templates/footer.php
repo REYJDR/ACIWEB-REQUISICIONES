90434c4fdaa039dbe5f75cd55643465a171777e1
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-04-05 10:29:17 -0500

    test

--- ACI_WEB/application/view/_templates/footer.php
+++ ACI_WEB/application/view/_templates/footer.php
@@ -4,9 +4,9 @@
 <?php
 sleep(1);
     //GET LAST SYNC
- echo   $date_db = $this->model->Query_value('PurOrdr_Header_Exp','LAST_CHANGE','order by LAST_CHANGE desc limit 1');	  
+    $date_db = $this->model->Query_value('PurOrdr_Header_Exp','LAST_CHANGE','order by LAST_CHANGE desc limit 1');	  
 
-    $date = $this->model->GetLocalTime(UTC,$date_db);
+    $date = $this->model->GetLocalTime('MTS',$date_db);
     //$dbHour = strtotime($date);
 
     $Nowdate = $this->model->GetLocalTime(UTC,date('Y-m-d H:i:s')); 
