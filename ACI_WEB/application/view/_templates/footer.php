a24a053106d9483bccae431e225a4e7bf13c059a
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-04-05 10:21:06 -0500

    test

--- ACI_WEB/application/view/_templates/footer.php
+++ ACI_WEB/application/view/_templates/footer.php
@@ -14,7 +14,7 @@
 
     //$dif = ($NowHour - $dbHour)/3600;
 
-    echo '<i style="font-weight:bold; color:red; font-size:12; ">'.$date.'-'.$Nowdate.'</i>';
+    echo '<i style="font-weight:bold; color:red; font-size:12; ">'.$date_db.'-'.$date.'-'.$Nowdate.'</i>';
 
     if ($dif >= 2){
 
