a191974af377e807279260969a4532804a68cae7
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-05 13:35:38 -0600

    Update req_crear.php

--- ACI_WEB/application/view/operaciones/req_crear.php
+++ ACI_WEB/application/view/operaciones/req_crear.php
@@ -431,7 +431,7 @@ var x=document.getElementById(UNIID).innerHTML;
 	
 	<legend><h4>Adjuntar soporte</h4></legend>
 
-	<input class=" col-lg-12" accept='file_extension' type="file" name="file" id="file" />
+	<input class=" col-lg-12" accept='png|jpg|pdf|xlsz|xls' type="file" name="file" id="file" />
 
 
 	
