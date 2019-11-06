b5ba00444023cc7b6945aa61e06db6d580e49e84
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-06 12:16:54 -0600

    Update upload.php

--- ACI_WEB/public/soportes/upload.php
+++ ACI_WEB/public/soportes/upload.php
@@ -9,40 +9,39 @@ if (!file_exists($target_dir)) {
 }
 
 
-var_dump($_FILES["file"]);
 
 $process = '';
 
-$count=0;
-foreach ($_FILES["file"]["name"] as $filename) 
-{
-    $process .= $filename.'-';
 
-    $target_file = $target_dir . basename($filename);
-    $uploadOk = 1;
+$filename = $_FILES["file"]["name"] ;
+  
+$process .= $filename.'-';
+
+$target_file = $target_dir . basename($filename);
+$uploadOk = 1;
 
 
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        $process .= "  Sorry, file already exists.<br>";
-        $uploadOk = 0;
-    }
-
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        $process .= "  Sorry, your file was not uploaded.<br>";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["file"]["tmp_name"][$count], $target_file)) {
-            $process .= "  The file ". basename($filename). " has been uploaded.";
-        } else {
-            $process .= "  Sorry, there was an error uploading your file.<br>";
-        }
-    }
-
-  $count=$count + 1;
-
+// Check if file already exists
+if (file_exists($target_file)) {
+    $process .= "  Sorry, file already exists.<br>";
+    $uploadOk = 0;
 }
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    $process .= "  Sorry, your file was not uploaded.<br>";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        $process .= "  The file ". basename($filename). " has been uploaded.";
+    } else {
+        $process .= "  Sorry, there was an error uploading your file.<br>";
+    }
+}
+
+
+
+
 echo  '/'.$process;
 
 
