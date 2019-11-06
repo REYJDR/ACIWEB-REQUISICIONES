6851f9ab1778b88aaca92578588de372e2eaeac8
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-05 18:02:55 -0600

    Update upload.php

--- ACI_WEB/public/soportes/upload.php
+++ ACI_WEB/public/soportes/upload.php
@@ -21,10 +21,10 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+// if ($_FILES["fileToUpload"]["size"] > 500000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
 // Allow certain file formats
 // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 // && $imageFileType != "gif" && $imageFileType != "doc" && $imageFileType != "docx" ) {
