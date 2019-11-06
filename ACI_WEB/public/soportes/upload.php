59832eaa7c5ded57be81de50e742d38e0028d1aa
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-06 11:32:47 -0600

    Update upload.php

--- ACI_WEB/public/soportes/upload.php
+++ ACI_WEB/public/soportes/upload.php
@@ -9,32 +9,31 @@ if (!file_exists($target_dir)) {
 }
 
 
-
+var_dump($_FILES["fileToUpload"]["name"]);
 
 $count=0;
 foreach ($_FILES["fileToUpload"]["name"] as $filename) 
 {
 
     $target_file = $target_dir . basename($filename);
-    echo  $target_file;
     $uploadOk = 1;
 
 
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.<br>";
+        echo "  Sorry, file already exists.<br>";
         $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.<br>";
+        echo "  Sorry, your file was not uploaded.<br>";
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$count], $target_file)) {
-            echo "The file ". basename($filename). " has been uploaded.<br>";
+            echo "  The file ". basename($filename). " has been uploaded.";
         } else {
-            echo "Sorry, there was an error uploading your file.<br>";
+            echo "  Sorry, there was an error uploading your file.<br>";
         }
     }
 
