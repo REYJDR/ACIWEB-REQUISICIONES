89b1879d6dc4655a2cb0cd29c98f2a1834e26d3f
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-08 18:21:28 -0600

    Update download.php

--- ACI_WEB/public/soportes/download.php
+++ ACI_WEB/public/soportes/download.php
@@ -5,17 +5,17 @@ $file = trim($_REQUEST['file']);
 
 $dir  = getcwd()."/".$req."/".$file;
 echo $dir;
-// if (file_exists($dir)) {
-//     header('Content-Description: File Transfer');
-//     header('Content-Type: application/octet-stream');
-//     header('Content-Disposition: attachment; filename="'.basename($dir).'"');
-//     header('Expires: 0');
-//     header('Cache-Control: must-revalidate');
-//     header('Pragma: public');
-//     header('Content-Length: ' . filesize($dir));
-//     readfile($dir);
-//     exit;
-// }
+if (file_exists($dir)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($dir));
+    readfile($dir);
+    exit;
+}
 
 
 ?>
