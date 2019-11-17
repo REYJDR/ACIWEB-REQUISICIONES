632e2307d3f3f67f0d0937a78f41ff6f7df43239
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-11-17 11:56:42 -0600

    Update bridge_query.php

--- ACI_WEB/application/controller/bridge_query.php
+++ ACI_WEB/application/controller/bridge_query.php
@@ -5931,7 +5931,7 @@ $table.= '<button type="button" class="close" aria-label="Close" onclick="CLOSE_
   $table.='</tbody></table>';
 
   $table.= '<div style="float:right;" class="col-md-2">
-  <a href="'.URL.'index.php?url=ges_requisiciones/req_copy/'.$value->{'PurchaseID'}.'/OC"  class="btn btn-block btn-secondary btn-icon btn-icon-standalone btn-icon-standalone-right btn-single text-left">
+  <a href="'.URL.'index.php?url=ges_requisiciones/req_copy/'.$inv.'/OC"  class="btn btn-block btn-secondary btn-icon btn-icon-standalone btn-icon-standalone-right btn-single text-left">
      <img  class="icon" src="img/File Send.png" />
     <span>Copiar</span>
   </a>
