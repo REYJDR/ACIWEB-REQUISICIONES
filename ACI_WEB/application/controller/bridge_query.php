49971d26cb86e706003a75aea1aef102ebecb24d
GDR Solutions <REINALDO.DAOU@GMAIL.COM>
2026-09-10 12:56:02 -0600

    diag: use include instead of require_once to see failure mode

--- ACI_WEB/application/controller/bridge_query.php
+++ ACI_WEB/application/controller/bridge_query.php
@@ -5124,7 +5124,11 @@ echo 'DIAG:IS_READABLE='.(is_readable($phpMailerPath)?'1':'0').';';
 echo 'DIAG:REALPATH='.realpath($phpMailerPath).';';
 echo 'DIAG:FILEPERMS='.substr(sprintf('%o', fileperms($phpMailerPath)), -4).';';
 
-require_once $phpMailerPath;
+echo 'DIAG:BEFORE_INCLUDE;';
+$includeResult = include $phpMailerPath;
+echo 'DIAG:AFTER_INCLUDE='.var_export($includeResult, true).';';
+echo 'DIAG:FUNC_EXISTS='.(function_exists('PHPMailerAutoload')?'1':'0').';';
+echo 'DIAG:CLASS_EXISTS='.(class_exists('PHPMailer', false)?'1':'0').';';
 
 echo 'DIAG:REQUIRE_OK;';
 
